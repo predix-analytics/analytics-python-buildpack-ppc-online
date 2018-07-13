bca5dce9b672f88536320ce5330408f85e362c52
predix-analytics <36939641+predix-analytics@users.noreply.github.com>
2018-07-13 14:14:48 -0700

    Update supply.go

--- src/python/supply/supply.go
+++ src/python/supply/supply.go
@@ -272,8 +272,7 @@ func (s *Supplier) InstallPython() error {
 	if err != nil {
 		return err
 	}
-	
-	tar xzvf python27FilePath -C $pythonInstallDir
+	s.unzip(python27FilePath, pythonInstallDir)
 	
 	if err := s.Stager.LinkDirectoryInDepDir(filepath.Join(pythonInstallDir, "bin"), "bin"); err != nil {
 		return err
@@ -299,6 +298,49 @@ func (s *Supplier) InstallPython() error {
 	return nil
 }
 
+func unzip(src, dest string) error {
+    r, err := zip.OpenReader(src)
+    if err != nil {
+        return err
+    }
+    defer r.Close()
+
+    for _, f := range r.File {
+        rc, err := f.Open()
+        if err != nil {
+            return err
+        }
+        defer rc.Close()
+
+        fpath := filepath.Join(dest, f.Name)
+        if f.FileInfo().IsDir() {
+            os.MkdirAll(fpath, f.Mode())
+        } else {
+            var fdir string
+            if lastIndex := strings.LastIndex(fpath,string(os.PathSeparator)); lastIndex > -1 {
+                fdir = fpath[:lastIndex]
+            }
+
+            err = os.MkdirAll(fdir, f.Mode())
+            if err != nil {
+                log.Fatal(err)
+                return err
+            }
+            f, err := os.OpenFile(
+                fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+            if err != nil {
+                return err
+            }
+            defer f.Close()
+
+            _, err = io.Copy(f, rc)
+            if err != nil {
+                return err
+            }
+        }
+    }
+    return nil
+}
 func (s *Supplier) RewriteShebangs() error {
 	files, err := filepath.Glob(filepath.Join(s.Stager.DepDir(), "bin", "*"))
 	if err != nil {
